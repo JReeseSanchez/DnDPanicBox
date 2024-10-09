13c4f703f64e7e7b6fa8fe478e8f073c272ba2c6
JReeseSanchez <jaiden.sanchez@ucalgary.ca>
2024-10-08 18:06:34 -0600

    Setup

---
+++ dnd-panic-box/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <header>
+        <h1>D&D Panic Box</h1>
+        <h3>What did you not prepare for?</h3>
+      </header>
+      <body>
+        <ul>
+          <li><button></button></li>
+
+        </ul>
+      </body>
+    </>
+  );
+}
+
+export default App;
